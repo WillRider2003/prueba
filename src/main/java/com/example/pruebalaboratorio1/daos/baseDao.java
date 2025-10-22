662ba9375e82ed7646ae6213b3b7506d179783f8
WillRider2003 <a20221862@pucp.edu.pe>
2025-10-22 15:36:21 -0500

    Update baseDao.java

--- src/main/java/com/example/pruebalaboratorio1/daos/baseDao.java
+++ src/main/java/com/example/pruebalaboratorio1/daos/baseDao.java
@@ -1,13 +1,26 @@
+// baseDao.java
 package com.example.pruebalaboratorio1.daos;
-
-import com.example.pruebalaboratorio1.beans.pelicula;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class baseDao {
-
-
+    
+    protected Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        
+        String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=America/Lima";
+        String username = "root";
+        String password = "root";
+        
+        return DriverManager.getConnection(url, username, password);
+    }
+    
+    // Método abstracto para validar borrado
+    public abstract boolean validarBorrado(Object entity);
 }
-
